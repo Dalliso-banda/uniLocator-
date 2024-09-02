0dff4eaad1faf385d41e01cd16d040213765cc9e
Frendolsk <112899523+Dalliso-banda@users.noreply.github.com>
2024-09-02 06:15:37 +0200

    Add files via upload
    First commit

---
+++ index.js
@@ -0,0 +1,54 @@
+/*written by frendolsk.com. api by http://universities.hipolabs.com.*/
+
+/*i suck at variable naming*/
+/*0976557875 whatsapp*/
+alert('some countries are not in the database')
+let country;
+const countrySelector= document.getElementById('country');
+let search= document.querySelector('input');
+let display= document.getElementById('displayField');
+countrySelector.onchange=(event)=>{
+   country=event.target.value;
+   console.log(country)
+   if(country){
+      getUnis(country);
+   }
+   else{
+       console.log(new Error)
+   }
+}
+
+  
+
+   function getUnis(country){
+let API = `http://universities.hipolabs.com/search?country=${country}`;
+fetch(API)
+.then(res=>res.json()).then(data=>{
+   let results= data.map((uni)=>{
+       return uni;
+   })
+   
+   console.log(results)
+    search.oninput=(event)=>{
+      const value=event.target.value;
+      display.innerHTML="";
+     results.forEach((result)=>{
+              if(result.name.toLowerCase().trim().includes(value.toLowerCase().trim())){
+            
+         let name =document.createElement('a');
+         name.innerHTML= result.name;
+         name.href=result.web_pages;
+         display.appendChild(name)
+         name.onclick=()=>{
+         if(!result.web_pages){
+             alert('link could not be found')
+         }
+         }
+         
+      }
+      
+      })
+    }
+}).catch(error=>console.log(error))
+}
+/*functional programming*/
